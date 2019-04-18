4774bdadf0fbc802b61695681686edc112c4bbfe
NotoriousRebel <matt12299@yahoo.com>
2019-04-18 15:25:18 -0400

    Added batch file to automate sme ddefense also organized script and modified applocker and firewall functions.

---
+++ nice.bat
@@ -0,0 +1,83 @@
+::
+::#######################################################################
+::
+:: Change file associations to protect against common ransomware attacks
+:: Note that if you legitimately use these extensions, like .bat, you will now need to execute them manually from cmd or powershell
+:: Alternatively, you can right-click on them and hit 'Run as Administrator' but ensure it's a script you want to run :) 
+:: ---------------------
+ftype htafile="%SystemRoot%\system32\NOTEPAD.EXE" "%1"
+ftype WSHFile="%SystemRoot%\system32\NOTEPAD.EXE" "%1"
+ftype batfile="%SystemRoot%\system32\NOTEPAD.EXE" "%1"
+::
+::#######################################################################
+::
+:: Enable ASR rules in Win10 1709 ExploitGuard to mitigate Offic malspam
+:: Blocks Office childprocs, Office proc injection, Office win32 api calls & executable content creation
+:: Note these only work when Defender is your primary AV
+:: Source: https://www.darkoperator.com/blog/2017/11/11/windows-defender-exploit-guard-asr-rules-for-office
+:: ---------------------
+::%programfiles%\"Windows Defender"\MpCmdRun.exe -RestoreDefaults
+:: Block Office applications from creating child processes
+powershell.exe Add-MpPreference -AttackSurfaceReductionRules_Ids D4F940AB-401B-4EFC-AADC-AD5F3C50688A -AttackSurfaceReductionRules_Actions Enabled
+:: Block Office applications from injecting code into other processes
+powershell.exe Add-MpPreference -AttackSurfaceReductionRules_Ids 75668C1F-73B5-4CF0-BB93-3ECF5CB7CC84 -AttackSurfaceReductionRules_Actions enable
+:: Block Win32 API calls from Office macro
+powershell.exe Add-MpPreference -AttackSurfaceReductionRules_Ids 92E97FA1-2EDF-4476-BDD6-9DD0B4DDDC7B -AttackSurfaceReductionRules_Actions enable
+:: Block Office applications from creating executable content
+powershell.exe Add-MpPreference -AttackSurfaceReductionRules_Ids '3B576869-A4EC-4529-8536-B80A7769E899' -AttackSurfaceReductionRules_Actions enable
+:: Block execution of potentially obfuscated scripts
+powershell.exe Add-MpPreference -AttackSurfaceReductionRules_Ids 5BEB7EFE-FD9A-4556-801D-275E5FFC04CC -AttackSurfaceReductionRules_Actions Enabled
+:: Block executable content from email client and webmail
+powershell.exe Add-MpPreference -AttackSurfaceReductionRules_Ids BE9BA2D9-53EA-4CDC-84E5-9B1EEEE46550 -AttackSurfaceReductionRules_Actions Enabled
+:: Block JavaScript or VBScript from launching downloaded executable content
+powershell.exe Add-MpPreference -AttackSurfaceReductionRules_Ids D3E037E1-3EB8-44C8-A917-57927947596D -AttackSurfaceReductionRules_Actions Enabled
+::
+::#######################################################################
+:: Enable Cloud functionality of Windows Defender
+::#######################################################################
+powershell.exe Set-MpPreference -MAPSReporting Advanced
+powershell.exe Set-MpPreference -SubmitSamplesConsent Always
+
+::
+:: General OS hardening
+:: Disables DNS multicast, smbv1, netbios, powershellv2
+:: Enables UAC and sets to always notify
+:: ---------------------
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows NT\DNSClient" /v EnableMulticast /t REG_DWORD /d 0 /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\LanmanServer\Parameters" /v SMB1 /t REG_DWORD /d 0 /f
+reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v EnableLUA /t REG_DWORD /d 1 /f
+reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v ConsentPromptBehaviorAdmin /t REG_DWORD /d 2 /f
+net stop WinRM
+wmic /interactive:off nicconfig where TcpipNetbiosOptions=1 call SetTcpipNetbios 2
+powershell.exe Disable-WindowsOptionalFeature -Online -FeatureName smb1protocol
+powershell.exe Disable-WindowsOptionalFeature -Online -FeatureName MicrosoftWindowsPowerShellV2
+powershell.exe Disable-WindowsOptionalFeature -Online -FeatureName MicrosoftWindowsPowerShellV2Root
+::
+::#######################################################################
+::
+:: Harden lsass to help protect against credential dumping (mimikatz)
+:: Configures lsass.exe as a protected process and disabled wdigest
+:: Source: https://technet.microsoft.com/en-us/library/dn408187(v=ws.11).aspx
+:: ---------------------
+reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\LSASS.exe" /v AuditLevel /t REG_DWORD /d 00000008 /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Lsa" /v RunAsPPL /t REG_DWORD /d 00000001 /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\SecurityProviders\WDigest" /v UseLogonCredential /t REG_DWORD /d 0 /f
+::
+::#######################################################################
+::
+:: Enable Windows Firewall and configure some advanced options
+:: Block Win32 binaries from making netconns when they shouldn't
+:: ---------------------
+NetSh Advfirewall set allprofiles state on
+Netsh.exe advfirewall firewall add rule name="Block Notepad.exe netconns" program="%systemroot%\system32\notepad.exe" protocol=tcp dir=out enable=yes action=block profile=any
+Netsh.exe advfirewall firewall add rule name="Block regsvr32.exe netconns" program="%systemroot%\system32\regsvr32.exe" protocol=tcp dir=out enable=yes action=block profile=any
+Netsh.exe advfirewall firewall add rule name="Block calc.exe netconns" program="%systemroot%\system32\calc.exe" protocol=tcp dir=out enable=yes action=block profile=any
+Netsh.exe advfirewall firewall add rule name="Block mshta.exe netconns" program="%systemroot%\system32\mshta.exe" protocol=tcp dir=out enable=yes action=block profile=any
+Netsh.exe advfirewall firewall add rule name="Block wscript.exe netconns" program="%systemroot%\system32\wscript.exe" protocol=tcp dir=out enable=yes action=block profile=any
+Netsh.exe advfirewall firewall add rule name="Block cscript.exe netconns" program="%systemroot%\system32\cscript.exe" protocol=tcp dir=out enable=yes action=block profile=any
+Netsh.exe advfirewall firewall add rule name="Block runscripthelper.exe netconns" program="%systemroot%\system32\runscripthelper.exe" protocol=tcp dir=out enable=yes action=block profile=any
+
+:: Block all inbound connections on Public profile
+netsh advfirewall set publicprofile firewallpolicy blockinboundalways,allowoutbound
+:: Enable Windows Defender Network Protection
+powershell.exe Set-MpPreference -EnableNetworkProtection Enabled
--- rush.ps1
+++ rush.ps1
@@ -9,44 +9,50 @@ Write-Verbose -Message "**********"
 Write-Verbose -Message "TRY HARDER"
 Write-Verbose -Message "**********"
 
-function build_wall{
+function build_wall([String]$choice){
 	#while(1){
 		Write-Verbose -Message "Putting old rules into rules.txt!!!!"
 		Get-NetFirewallRule | Out-File -FilePath "C:\Users\$($env:USERNAME)\Desktop\Storage\rules.txt" -NoClobber
 		Write-Verbose -Message "Restoring firewall rules to default"
+		
 		netsh advfirewall reset
 		netsh advfirewall set allprofiles state on
 		netsh advfirewall firewall delete rule name=all
-		netsh advfirewall set allprofiles firewallpolicy blockinbound,blockoutbound
+		netsh advfirewall set allprofiles firewallpolicy,blockinbound,blockoutbound
 		Write-Verbose -Message "allow chrome!"
 		netsh advfirewall firewall add rule Name="Chrome in" Program="C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" Action=allow Dir=in
 		netsh advfirewall firewall add rule Name="Chrome out" Program="C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" Action=allow Dir=Out
 		#Remove-NetFirewallRule -All 
-		Write-Verbose -Message "*****************************"
-		Write-Verbose -Message "BUILDING WALL"
-		Write-Verbose -Message "*****************************"
-		for($num = 21; $num -lt 2000; $num++){		
-		#depending on box we don't want to block certain ports
-		if($num -eq 80 -OR $num -eq 443 -OR $num -eq 53 -OR $num -eq 514){
-			continue
+		netsh advfirewall firewall add rule name="ICMP Allow incoming V4 echo request" protocol=icmpv4:any,any dir=in action=allow
+		netsh advfirewall firewall add rule name="Allow ICMP out" protocol=icmpv4:any,any dir=out action=allow
+		netsh advfirewall firewall add rule name="Open Port 53" dir=in action=allow protocol=UDP localport=53
+		netsh advfirewall firewall add rule name="Open Port 53" dir=out action=allow protocol=UDP localport=53
+		netsh advfirewall firewall add rule name="Open Port 53" dir=in action=allow protocol=TCP localport=53
+		netsh advfirewall firewall add rule name="Open Port 53" dir=out action=allow protocol=TCP localport=53
+		netsh advfirewall firewall add rule name="Open Port 67" dir=in action=allow protocol=UDP localport=67
+		netsh advfirewall firewall add rule name="Open Port 67" dir=out action=allow protocol=UDP localport=67
+		netsh advfirewall firewall add rule name="Open Port 68" dir=in action=allow protocol=UDP localport=68
+		netsh advfirewall firewall add rule name="Open Port 68" dir=out action=allow protocol=UDP localport=68
+		
+		if($choice -eq '1'){
+		
 		}
-		Write-Verbose -Message "Blocking port + $num"
-		Write-Verbose -Message "Blocking TCP"
-		netsh advfirewall firewall add rule name="Blocktcp_in + $num" protocol=TCP dir=in localport=$num action=block
-		netsh advfirewall firewall add rule name="Blocktcp_out + $num" protocol=TCP dir=out localport=$num action=block
-		Write-Verbose -Message "Blocking UDP"
-		netsh advfirewall firewall add rule name="Blocktcp_in + $num" protocol=UDP dir=in localport=$num action=block
-		netsh advfirewall firewall add rule name="Blocktcp_out + $num" protocol=UDP dir=out localport=$num action=block
+		
+		if($choice -eq '2'){
+		
 		}
+		
+	Write-Verbose -Message "Configuring logs."
+	netsh advfirewall set currentprofile logging filename "C:\Users\$(env:USERNAME)\Desktop\Storage\firewall.log"
+	netsh advfirewall set currentprofile logging maxfilesize 4096
+	netsh advfirewall set currentprofile logging droppedconnections enable
+	netsh advfirewall set currentprofile logging allowedconnections enable
 	
-		#Sleep for 180 seconds before running again
-		#Start-Sleep -s 180
-	#}
 }
 
 function stop_process{ 
 	Write-Verbose -Message "Dummping running processes into proccess.txt"
-	tasklist | Out-File "processes.txt"
+	tasklist | Out-File "C:\Users\$($env:USERNAME)\Desktop\Storage\processes.txt"
 	$tasklist = tasklist.exe
 	$tasklist = $tasklist.Split(" ") 
 	$truetaskList =  @()
@@ -105,18 +111,18 @@ function change_users{
 
 function scan{
 	Write-Verbose -Message "Starting quick scan!!!!!!!"
+	Set-MpPreference -ScanParameters 2 -ScanScheduleDay 0 -ScanScheduleQuickScanTime 1 -UnknownThreatDefaultAction "Quarantine" -SevereThreatDefaultAction "Quarantine" -HighThreatDefaultAction "Quarantine" -LowThreatDefaultAction "Quarantine" -ModerateThreatDefaultAction "Quarantine" -CheckForSignaturesBeforeRunningScan 1 -DisableRealtimeMonitoring 0
 	Try{
-		Set-MpPreference -ScanParameters 2 -ScanScheduleDay 0 -ScanScheduleQuickScanTime 1 -UnknownThreatDefaultAction "Quarantine" -SevereThreatDefaultAction "Quarantine" -HighThreatDefaultAction "Quarantine" -LowThreatDefaultAction "Quarantine" -ModerateThreatDefaultAction "Quarantine" -CheckForSignaturesBeforeRunningScan 1 -DisableRealtimeMonitoring 0
 		Start-MpScan -ThrottleLimit 0 -ScanType 1
-		Write-Verbose -Message "Sleeping for 30 seconds then running full scan!"
-		Start-Sleep 30
+		#Write-Verbose -Message "Sleeping for 30 seconds then running full scan!"
+		#Start-Sleep 30
 		Start-MpScan -ThrottleLimit 0 -ScanType 2
 	}
 	Catch{
 		Try{
 			C:\"Program Files"\"Windows Defender"\MpCmdRun.exe -Scan -ScanType 1
-			Write-Verbose -Message "Sleeping for 60 seconds then running full scan!"
-			Start-Sleep 30
+			#Write-Verbose -Message "Sleeping for 60 seconds then running full scan!"
+			#Start-Sleep 30
 			C:\"Program Files"\"Windows Defender"\MpCmdRun.exe -Scan -ScanType 2
 		 }
 		 Catch{
@@ -127,21 +133,37 @@ function scan{
 }
 
 function dump_tasks{
+	$cur = $env:USERNAME
 	Write-Verbose "Putting scheduledtasks into tasks.txt"
-	Get-ScheduledTask | Out-File "tasks.txt"
+	tasklist | Out-File "C:\Users\$cur\Storage\tasks.txt"
 	Write-Verbose "Putting scheduledtask information into tasksinfo.txt"
-	Get-ScheduledTask | Get-ScheduledTaskInfo | Out-File "tasksinfo.txt"
+	Get-ScheduledTask | ? state -eq running | Get-ScheduledTaskInfo | Out-File "C:\Users\$cur\Storage\running_scheduled_tasks.txt"
 }
 
 function app_lock{
-	Write-Verbose -Message "Dumping local policy info" -Verbose
-	Get-AppLockerPolicy -Local | Out-File "applocker_info.txt" 
-	Write-Verbose -Message "dumping system32 applocker file info" -Verbose
-	Get-AppLockerFileInformation -Directory C:\Windows\System32\ -Recurse -FileType Exe, Script | Out-File "sys32 info"
-	Write-Verbose -Message "Applying new app locker policy for google"  -Verbose
-	Get-AppLockerFileInformation -Directory "C:\Program Files (x86)\Google\" -Recurse -FileType Exe,DLL | New-AppLockerPolicy -RuleType Publisher, Path -User Everyone -XML -Optimize -IgnoreMissingFileInformation| Out-File "google.xml" | Set-AppLockerPolicy -XMLPolicy "google.xml"
-	Write-Verbose -Message "Applying new app locker policy for scripts in user John folder"  -Verbose
-	Get-AppLockerFileInformation -Directory C:\Users\John -Recurse -FileType Script | New-AppLockerPolicy -RuleType Publisher, Path -User Everyone -IgnoreMissingFileInformation -XML -Optimize| Out-File "ps_policy.xml" | Set-AppLockerPolicy -XMLPolicy "ps_policy.xml"
+	sc config "AppIDSvc" start=auto; net start "AppIDSvc"
+	$first_policy = (IWR -Uri "https://raw.githubusercontent.com/MotiBa/AppLocker/master/Policies/AppLocker-Block-Paths.xml").Content
+	$second_policy = (IWR -Uri "https://github.com/MotiBa/AppLocker/blob/master/Policies/AppLocker-Block-Publishers.xml").Content
+	$deny = '<AppLockerPolicy Version="1"><RuleCollection Type="Exe, DLL, Script" EnforcementMode="NotConfigured">
+	<FilePathRule Id="31B2F340-016D-11D2-945F-00C04FB984F9" Name="%SYSTEM32%\*" Description="" 10 UserOrGroupSid="S-1-5-21-3165297888-301567370-576410423-13" 
+	Action="Deny"><Conditions><FilePathCondition Path="%SYSTEM32%\*" /></Conditions></FilePathRule></RuleCollection> 
+	</AppLockerPolicy>'
+	$allow = '<AppLockerPolicy Version="1"><RuleCollection Type="Exe, DLL, Script" EnforcementMode="NotConfigured">
+	<FilePathRule Id="31B2F340-016D-11D2-945F-00C04FB984F9" Name="%SYSTEM32%\*" Description="" 10 UserOrGroupSid="S-1-5-21-3165297888-301567370-576410423-13" 
+	Action="Allow"><Conditions><FilePathCondition Path="%SYSTEM32%\*" /></Conditions></FilePathRule></RuleCollection> 
+	</AppLockerPolicy>'
+	Get-AppLockerFileInformation -Directory C:\Windows\system32\ -Recurse -FileType exe, dll, script | New-AppLockerPolicy -RuleType Path -User Everyone -Optimize -XML $deny
+	Get-AppLockerFileInformation -Directory C:\Windows\system32\ -Recurse -FileType exe, dll, script | New-AppLockerPolicy -RuleType Path -User "$($env:USERNAME), Kiwi" -Optimize -XML $allow
+	# make sure app locker is running!
+	#IWR(-Uri "http://tinyurl.com/y5fwusjg" -MaximumRedirection 2).Content
+	#Write-Verbose -Message "Dumping local policy info" -Verbose
+	#Get-AppLockerPolicy -Local | Out-File "applocker_info.txt" 
+	#Write-Verbose -Message "dumping system32 applocker file info" -Verbose
+	#Get-AppLockerFileInformation -Directory C:\Windows\System32\ -Recurse -FileType Exe, Script | Out-File "sys32 info"
+	#Write-Verbose -Message "Applying new app locker policy for google"  -Verbose
+	#Get-AppLockerFileInformation -Directory "C:\Program Files (x86)\Google\" -Recurse -FileType Exe,DLL | New-AppLockerPolicy -RuleType Publisher, Path -User Everyone -XML -Optimize -IgnoreMissingFileInformation| Out-File "google.xml" | Set-AppLockerPolicy -XMLPolicy "google.xml"
+	#Write-Verbose -Message "Applying new app locker policy for scripts in user John folder"  -Verbose
+	#Get-AppLockerFileInformation -Directory C:\Users\John -Recurse -FileType Script | New-AppLockerPolicy -RuleType Publisher, Path -User Everyone -IgnoreMissingFileInformation -XML -Optimize| Out-File "ps_policy.xml" | Set-AppLockerPolicy -XMLPolicy "ps_policy.xml"
 }
 
 function install_packages{	
@@ -165,7 +187,7 @@ function install_chocolate{
 function fruit_user{
 	Write-Verbose -Message "Adding user kiwi!"
 	$Password = (ConvertTo-SecureString -AsPlainText "KiwisAreNotFun" -Force)
-	New-LocalUser "kiwi" -Password $Password -FullName "Kiwis Suck" -Description "eats fruit, likes Ben Delpy"
+	New-LocalUser "Kiwi" -Password $Password -FullName "Kiwi" -Description "eats fruit, likes Ben Delpy"
 	Add-LocalGroupMember -Group "Administrators" -Member "kiwi"
 }
 
@@ -198,7 +220,7 @@ function read_history{
 }
 
 function stop_scripts{
-	$path = "HKLM:\SOFTWARE\Microsoft\Windows Script Host\"
+	$path = "HKLM:\SOFTWARE\Microsoft\Windows Script Host\Settings"
 	Try{
 		New-ItemProperty -Path $path -Name "Enabled" -Value 0 -PropertyType "DWord"
 	}
@@ -210,14 +232,13 @@ function stop_scripts{
 		}
 		Catch{
 			Write-Verbose -Message "Could not change registry value for Windows Script Host do it manually here! $path"
-			Write-Host "Could not change registry value for Windows Script Host do it manually here! $($path)"
+			Write-Host "Could not change registry value for Windows Script Host do it manually here! $path"
 
 		}
 	}
 }
 
 function lockdown_pol{
-
 	Write-Verbose -Message "Setting lockdown policy" -verbose
 	Try{
 		[Environment]::SetEnvironmentVariable('__PSLockdownPolicy', '4', 'Machine')
@@ -227,24 +248,23 @@ function lockdown_pol{
 		Write-Verbose -Message $string_err -verbose
 	}
 }
-function main{
-	Clear
-	#[CmdletBinding()] 
-    $UserAccount = Get-LocalUser -Name "Administrator"
+
+function remove_junk{
+	$UserAccount = Get-LocalUser -Name "Administrator"
 	Try{
 		Write-Verbose -Message "Disabling SMB1" -Verbose
-		Disable-WindowsOptionalFeature -Online -FeatureName 'SMB1Protocol' -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -NoRestart | Out-Null
-		Write-Verbose -Message "Disabling SMB2" -Verbose
-		Set-SmbServerConfiguration -EnableSMB2Protocol $false
-		Write-Verbose -Message"Disabling SMB3" -Verbose
-		Set-SmbServerConfiguration -EnableSMB3Protocol $false 
+		Disable-WindowsOptionalFeature -Online -FeatureName 'SMB1Protocol' -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -NoRestart 
+		#Write-Verbose -Message "Disabling SMB2" -Verbose
+		#Set-SmbServerConfiguration -EnableSMB2Protocol $false
+		#Write-Verbose -Message"Disabling SMB3" -Verbose
+		#Set-SmbServerConfiguration -EnableSMB3Protocol $false 
 	}
 	Catch{
 		$string_err = $_ | Out-String
 		Write-Verbose -Message $string_err -verbose
 	}
 	Write-Verbose -Message "Disabling RDP!!!" -verbose
-	try{
+	Try{
 			Set-ItemProperty -Path "HKLM:\System\CurrentControlSet\Control\Terminal Server" -Name "fDenyTSConnections" -Value 0
 			Write-Verbose -Message "RDP Disabled"
 		}
@@ -252,8 +272,25 @@ function main{
 		$string_err = $_ | Out-String
 		Write-Verbose -Message $string_err -verbose
 	}
+	Write-Verbose -Message "Removing Powershellv2" -verbose
+	Try{
+		Disable-WindowsOptionalFeature -Online -FeatureName MicrosoftWindowsPowerShellV2 -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -NoRestart
+	}
+	Catch{
+		$string_err = $_ | Out-String
+		Write-Verbose -Message $string_err -verbose
+	}
+}
+
+function main{
+	Clear
+	 -Scope LocalMachine
+	# Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
+	# (IWR -Uri "http://tinyurl.com/y5fwusjg" -MaximumRedirection 2 ).Content | IEX
+	#[CmdletBinding()] 
 	Write-Verbose -Message "Creating directory C:\Users\$($env:USERNAME)\Desktop\Storage"
 	New-Item -Path "C:\Users\$($env:USERNAME)\Desktop" -Name "Storage" -ItemType "directory"
+	remove_junk
 	install_chocolate
 	install_packages
 	dump_tasks
@@ -261,16 +298,11 @@ function main{
 	read_history
 	process_poker
 	fruit_user
-	#build_wall
+	app_lock
+	build_wall
 	stop_scripts
+	scan
 	lockdown_pol
-	#app_lock
-	#stop_process
-	#build_wall
-	#scan
-	# -Scope LocalMachine
-	# Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
-	# (IWR -Uri "http://tinyurl.com/y5fwusjg" -MaximumRedirection 2 ).Content | IEX
 }
 
 main
